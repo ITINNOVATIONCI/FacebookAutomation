0d622fd598564a10c8c9e3d17357c97c1cf91f1f
Yverson <boamathieu@yahoo.fr>
2016-05-11 00:05:10 +0000

    Update Mailer.cs

--- src/FacebookAutomation/Services/Mailer.cs
+++ src/FacebookAutomation/Services/Mailer.cs
@@ -19,9 +19,6 @@ namespace FacebookAutomation.Services
             {
                 /* CREDENTIALS
                  * ===================================================*/
-                //string sgUsername = "azure_da0713702f435dcc22edd7c82dd05341@azure.com";
-                //string sgPassword = "P@ssw0rd2012";
-
                 string sgUsername = "";
                 string sgPassword = "";
                 /* MAIL SETTINGS
